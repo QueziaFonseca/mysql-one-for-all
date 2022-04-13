2d86da74ea15c96b135cf02048d92404b683c821
quezia-lima <qfonseca1@gmail.com>
2022-04-13 20:33:32 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,13 @@
+SELECT
+  distinct u.user_name AS usuario,
+   COUNT(h.song_id) AS qtde_musicas_ouvidas,
+   ROUND((SUM(s.duration_seconds) / 60), 2) AS total_minutos
+FROM SpotifyClone.user AS u
+INNER JOIN SpotifyClone.history AS h
+	ON u.user_id = h.user_id
+    INNER JOIN SpotifyClone.song AS s
+	ON s.song_id = h.song_id
+GROUP BY user_name
+;
+-- SUM soma
+-- ROUND arredonda com o numero de casas decimais que eu escolher
