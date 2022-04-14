b812cfcbfb838b59c0b4cf596a22d0467527750a
quezia-lima <qfonseca1@gmail.com>
2022-04-14 00:40:58 -0300

    Desafio 8 ok

--- desafio8.sql
+++ desafio8.sql
@@ -1,8 +1,10 @@
-SELECT ar.artist_name AS artista,
-al.album_name AS album
-FROM SpotifyClone.artist AS ar
-INNER JOIN SpotifyClone.album AS al 
-ON ar.artist_id = al.artist_id
-WHERE ar.artist_name = 'Walter Phoenix'
-ORDER BY album
-;
+SELECT
+  ar.artist_name AS artista,
+  al.album_name AS album
+FROM
+  SpotifyClone.artist AS ar
+  INNER JOIN SpotifyClone.album AS al ON ar.artist_id = al.artist_id
+WHERE
+  ar.artist_name = 'Walter Phoenix'
+ORDER BY
+  album;
