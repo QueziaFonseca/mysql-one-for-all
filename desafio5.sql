d2c319ebc9a0ccae3840046bbd3667753038554f
quezia-lima <qfonseca1@gmail.com>
2022-04-13 22:53:49 -0300

    Desafio 5

---
+++ desafio5.sql
@@ -0,0 +1,15 @@
+SELECT
+  so.song_name AS cancao,
+  COUNT(h.date) AS `reproducoes`
+FROM
+  SpotifyClone.song AS so
+  INNER JOIN SpotifyClone.history AS h ON so.song_id = h.song_id
+GROUP BY
+  so.song_name
+ORDER BY
+  reproducoes DESC,
+  cancao
+LIMIT
+  2;
+
+  -- LIMIT limita a quantidade de respostas ao numero escolhido
